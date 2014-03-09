4533a88fcc1612e0fe129d14a4a120dfd1c5f830
kazuki-matsuda <kazuki-matsuda@dac.co.jp>
2014-03-09 13:15:05 +0900

    delete importer plugin

--- redmine_importer/config/routes.rb
+++
@@ -1,3 +0,0 @@
-match '/importer/index', :to => 'importer#index', :via => [:get, :post]
-match '/importer/match', :to => 'importer#match', :via => [:get, :post]
-match '/importer/result', :to => 'importer#result', :via => [:get, :post]
